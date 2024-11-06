1a1c07f221759f6ea1b1b612362d88f6ddfafe8f
Romeo <romeoquinones4@gmail.com>
2024-11-06 17:50:47 +0800

    Add routing for side navigation

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
+const navRouter = require('./routes/navigationRoutes.js');
 
 const app = express();
 const port = 3000;
 
 /* Middlewares */
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', navRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/dashboard.html'));
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
