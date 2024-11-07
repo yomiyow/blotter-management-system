16848011cee2c380daaeac7a5f29dccb0c318b48
Romeo <romeoquinones4@gmail.com>
2024-11-07 09:54:57 +0800

    Add MySQL2 dependency and establish database connection

---
+++ models/db-connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise');
+
+async function connectToDatabase() {
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '2022100960Romeo',
+      database: 'blotter_db'
+    });
+    console.log('Database connected!')
+    return connection;
+
+  } catch (err) {
+    console.log('Database connection failed: ', err);
+    throw err;
+  }
+}
+
+connectToDatabase();
+
+module.exports = { connectToDatabase };
