3e0ac8e2f0e51c5b9bb6c0948415acc427b13182
Romeo <romeoquinones4@gmail.com>
2024-11-17 19:17:08 +0800

    Add transition effects and update navigation styles

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,30 +1,13 @@
-/*======================================================
-                Side navigation
-======================================================*/
 document.addEventListener('DOMContentLoaded', () => {
-
   const toggle = document.querySelector('.toggle-btn');
   const sideNav = document.querySelector('.side-nav');
+  const mainContent = document.querySelector('.main-content');
 
   toggle.addEventListener('click', () => {
     const isOpen = sideNav.classList.toggle('isOpen');
     sideNav.style.minWidth = isOpen ? '15rem' : '44px';
     sideNav.style.width = isOpen ? '15rem' : '44px';
+    mainContent.style.marginLeft = isOpen ? '15rem' : '44px';
     sideNav.classList.toggle('isClose', !isOpen);
   });
-
-  const menus = document.querySelectorAll('.menu-link');
-
-  menus.forEach((menu) => {
-    menu.addEventListener('click', (event) => {
-      // event.preventDefault();
-
-      menus.forEach((menu) => menu.classList.remove('isActive'));
-      menu.classList.add('isActive');
-
-      const pageURL = menu.getAttribute('href');
-      window.history.pushState({ pageURL }, '', pageURL);
-    });
-  });
 });
-
