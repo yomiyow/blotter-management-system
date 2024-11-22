f2907869b592e268b4e402c114f616aea344fcf3
Romeo <romeoquinones4@gmail.com>
2024-11-22 23:24:40 +0800

    Replace profile.js with header.js in multiple views

---
+++ public/scripts/header.js
@@ -0,0 +1,11 @@
+// show password event handler
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdown = document.querySelector('.dropdown-menu');
+  const profile = document.querySelector('.icon-wrapper');
+  profile.addEventListener('click', () => dropdown.classList.toggle('show'));
+
+  document.addEventListener('click', (event) => {
+    if (!event.target.closest('.icon-wrapper'))
+      dropdown.classList.remove('show');
+  });
+});
