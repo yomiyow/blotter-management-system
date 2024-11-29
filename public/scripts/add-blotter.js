c8b7cc22f32001c6940084669096441f9021b9c6
Romeo <romeoquinones4@gmail.com>
2024-11-29 15:42:33 +0800

    Refactor add-blotter.html, make form scrollable

--- public/scripts/add-blotter.js
+++ public/scripts/add-blotter.js
@@ -1,75 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const prevButton = document.querySelector('.js-prev-btn');
-  const nextButton = document.querySelector('.js-next-btn');
-  const submitButton = document.querySelector('.js-submit-btn');
-  const forms = document.querySelectorAll('.js-form-step');
-  const tabs = document.querySelectorAll('.js-tab');
-
-  let currentFormIndex = 0;
-  const COMPLAINANT_FORM_INDEX = 0;
-  const LAST_FORM_INDEX = forms.length - 1;
-  const ACTIVE_TAB_COLOR = '#007bff';
-  const ACTIVE_TEXT_COLOR = 'white';
-  const INACTIVE_TAB_COLOR = 'white';
-  const INACTIVE_TEXT_COLOR = '#444444';
-
-  function updateFormVisibility() {
-    forms.forEach((form, formIndex) => {
-      const isFormVisible = (formIndex === currentFormIndex);
-      form.style.display = isFormVisible ? 'flex' : 'none';
-    });
-
-    tabs.forEach((tab, index) => {
-      const isTabActive = (index === currentFormIndex);
-      tab.style.backgroundColor = isTabActive ? ACTIVE_TAB_COLOR : INACTIVE_TAB_COLOR;
-      tab.style.color = isTabActive ? ACTIVE_TEXT_COLOR : INACTIVE_TEXT_COLOR;
-    });
-  }
+  const form = document.querySelector('form');
 
   function collectFormData() {
     const formData = {};
-    forms.forEach((form) => {
-      const inputs = form.querySelectorAll('input, select, textarea');
-      inputs.forEach((input) => {
-        formData[input.name] = input.value;
-      });
+    const inputs = form.querySelectorAll('input, select, textarea');
+    inputs.forEach((input) => {
+      formData[input.name] = input.value;
     });
 
     return formData;
-  }
-
-  function isCurrentFormValid() {
-    const currentForm = forms[currentFormIndex];
-    const inputs = currentForm.querySelectorAll('input[required], select[required], textarea[required]');
-    let isValid = true;
-
-    inputs.forEach((input) => {
-      if (!input.value.trim()) {
-        isValid = false;
-      }
-    });
-
-    return isValid;
-  }
-
-  function setButtonEnableState(button, enable = true) {
-    if (enable) {
-      button.classList.remove('disabled');
-    } else {
-      button.classList.add('disabled');
-    }
-  }
-
-  function resetForm() {
-    currentFormIndex = 0;
-    const formFields = document.querySelectorAll('input, select, textarea');
-    formFields.forEach((formField) => {
-      formField.value = '';
-    });
-    updateFormVisibility();
-    setButtonEnableState(prevButton, false)
-    setButtonEnableState(nextButton);
-    submitButton.style.display = 'none';
   }
 
   let alertTimeout;
@@ -81,61 +20,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     alertTimeout = setTimeout(() => {
       alertElement.style.display = 'none';
-    }, 4000);
+    }, 5000);
   }
 
-  // Display complainant form by default
-  updateFormVisibility();
-
-  // Disable previous button by default
-  setButtonEnableState(prevButton, false);
-
-  // Event Listeners
-
-  prevButton.addEventListener('click', (event) => {
+  form.addEventListener('submit', async (event) => {
     event.preventDefault();
-
-    if (currentFormIndex > COMPLAINANT_FORM_INDEX) {
-      currentFormIndex--;
-      updateFormVisibility();
-
-      setButtonEnableState(nextButton);
-      submitButton.style.display = 'none';
-    }
-
-    if (currentFormIndex === COMPLAINANT_FORM_INDEX) {
-      setButtonEnableState(prevButton, false);
-    }
-  });
-
-  nextButton.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (!isCurrentFormValid()) {
-      showAlert(document.querySelector('.alert-warning'));
-      return;
-    }
-
-    if (currentFormIndex < LAST_FORM_INDEX) {
-      currentFormIndex++;
-      updateFormVisibility();
-
-      setButtonEnableState(prevButton);
-    }
-
-    if (currentFormIndex === LAST_FORM_INDEX) {
-      setButtonEnableState(nextButton, false);
-      submitButton.style.display = 'block';
-    }
-  });
-
-  submitButton.addEventListener('click', async (event) => {
-    event.preventDefault();
-
-    if (!isCurrentFormValid()) {
-      showAlert(document.querySelector('.alert-warning'));
-      return;
-    }
 
     try {
       const response = await fetch('/api/add-blotter', {
@@ -149,7 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
       const result = await response.json();
       if (result.success) {
         showAlert(document.querySelector('.alert-success'));
-        resetForm();
+        form.reset();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
       } else {
         alert(result.error);
       }
@@ -158,4 +48,5 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error('Error creating blotter: ', err);
     }
   });
-});
+})
+
