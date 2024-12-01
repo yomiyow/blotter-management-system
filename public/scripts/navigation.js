79eaf7ae69e576932004e0272a8ec04a4b083f64
Romeo <romeoquinones4@gmail.com>
2024-12-01 15:48:21 +0800

    Refactor side navigation. Implement state management for open/close functionality

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -3,11 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const sideNav = document.querySelector('.side-nav');
   const mainContent = document.querySelector('.main-content');
 
+  // Retrieve the state from localStorage and set the initial state
+  const savedState = localStorage.getItem('sideNavState');
+  sideNav.dataset.state = savedState;
+  const size = (savedState === 'open') ? '15rem' : '44px';
+
+  sideNav.style.minWidth = size;
+  sideNav.style.width = size;
+  mainContent.style.marginLeft = size; ``
+
   toggle.addEventListener('click', () => {
-    const isOpen = sideNav.classList.toggle('isOpen');
-    sideNav.style.minWidth = isOpen ? '15rem' : '44px';
-    sideNav.style.width = isOpen ? '15rem' : '44px';
-    mainContent.style.marginLeft = isOpen ? '15rem' : '44px';
-    sideNav.classList.toggle('isClose', !isOpen);
+    const isOpen = sideNav.dataset.state === 'open';
+    const newState = isOpen ? 'close' : 'open';
+    sideNav.dataset.state = newState;
+    const size = (newState === 'open') ? '15rem' : '44px';
+
+    sideNav.style.minWidth = size;
+    sideNav.style.width = size;
+    mainContent.style.marginLeft = size;
+
+    // Save the state to localStorage
+    localStorage.setItem('sideNavState', newState);
   });
 });
