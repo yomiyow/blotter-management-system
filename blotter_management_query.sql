71e2816c5db3cea5fce2fcfcf08c9c0ea3d148c8
Romeo <romeoquinones4@gmail.com>
2024-12-01 11:56:13 +0800

    Refactor titles in HTML files

--- blotter_management_query.sql
+++ blotter_management_query.sql
@@ -321,4 +321,58 @@ WHERE MONTH(date_time_reported) = MONTH(CURDATE());
 
 
 SELECT * FROM blotter; 
+
+SELECT
+	c.firstname AS comFirstname,
+	c.middlename AS comMiddlename,
+	c.lastname AS comLastname,
+	c.nickname AS comNickname,
+	c.age AS comAge,
+	c.gender AS comGender,
+	c.civil_status AS comCivilStatus,
+	c.citizenship AS comCitizenship,
+	c.birthdate AS comBirthdate,
+	c.birthplace AS comBirthplace,
+	c.occupation AS comOccupation,
+	c.province AS comProvince,
+	c.city AS comCity,
+	c.barangay AS comBarangay,
+	c.house_no_street AS comHouseNoStreet,
+	c.mobile_no AS comMobileNo,
+	c.tel_no AS comTelNo,
+	c.email AS comEmail,
+	s.firstname AS susFirstname,
+	s.middlename AS susMiddlename,
+	s.lastname AS susLastname,
+	s.nickname AS susNickname,
+	s.age AS susAge,
+	s.gender AS susGender,
+	s.civil_status AS susCivilStatus,
+	s.citizenship AS susCitizenship,
+	s.birthdate AS susBirthdate,
+	s.birthplace AS susBirthplace,
+	s.occupation AS susOccupation,
+	s.province AS susProvince,
+	s.city AS susCity,
+	s.barangay AS susBarangay,
+	s.house_no_street AS susHouseNoStreet,
+	s.mobile_no AS susMobileNo,
+	s.tel_no AS susTelNo,
+	s.email AS susEmail,
+	b.blotter_id,
+	b.street,
+	b.barangay,
+	b.narrative,
+	DATE_FORMAT(b.date_time_reported, '%b %e, %Y at %l:%i %p') AS date_time_reported,
+	b.date_time_incident,
+	b.category,
+	b.status
+FROM blotter b
+INNER JOIN blotter_complainant bc ON b.blotter_id = bc.blotter_id
+INNER JOIN complainant c ON bc.complainant_id = c.complainant_id
+INNER JOIN blotter_suspect bs ON b.blotter_id = bs.blotter_id
+INNER JOIN suspect s ON bs.suspect_id = s.suspect_id
+WHERE 
+	(COALESCE('', '') = '' OR b.category = '') AND
+    (COALESCE('New', '') = '' OR b.status = 'New');
 #---------------------------------------------------------------------------------------
