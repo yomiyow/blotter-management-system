46b0cedfd30931b073405c465be95a8ed0c1ebc1
Romeo <romeoquinones4@gmail.com>
2024-12-02 11:49:10 +0800

    Update header links in multiple views to navigate to the dashboard

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dropdown = document.querySelector('.dropdown-menu');
   const profile = document.querySelector('.icon-wrapper');
+
   profile.addEventListener('click', () => dropdown.classList.toggle('show'));
 
+  document.querySelector('a[href="/logout"]')
+    .addEventListener('click', () => sessionStorage.clear());
+
+  // Hide dropdown menu when clicking outside of the profile icon
   document.addEventListener('click', (event) => {
     if (!event.target.closest('.icon-wrapper'))
       dropdown.classList.remove('show');
   });
-
-  document.querySelector('a[href="/logout"]')
-    .addEventListener('click', () => sessionStorage.clear());
 });
